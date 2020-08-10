cebdf2eb920a706043e36225759b33dd4bb507e0
Dakota Coppage <dakotakcoppage@me.com>
2020-08-10 00:25:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,9 +78,9 @@ def adding_matz
         }
      }
   programmer_hash
-  [:yukihiro_matsumoto] = {
-   :known_for => "Ruby",
-   :languages => ["LISP", "C"]
+    [:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
   
 }
 
