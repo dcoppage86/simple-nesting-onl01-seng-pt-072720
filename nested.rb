6f15a729c76cdf2b4aaeba2e132df8c7a8ebe7a4
Dakota Coppage <dakotakcoppage@me.com>
2020-08-10 00:13:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,19 +21,19 @@ end
 
 def alan_kay_is_known_for
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
+ 		    :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
         :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
         :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
   programmer_hash[:alan_kay][:known_for]
 
 end
