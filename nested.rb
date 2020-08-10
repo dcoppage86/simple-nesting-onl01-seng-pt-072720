54c71f8bf9beb6deb8fadaaa0feb830bd2c7949d
Dakota Coppage <dakotakcoppage@me.com>
2020-08-10 00:10:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	 		{
+ 		{
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -35,7 +35,7 @@ def alan_kay_is_known_for
         }
      }
   programmer_hash[:alan_kay]
-	}
+
 end
 
 def dennis_ritchies_language
